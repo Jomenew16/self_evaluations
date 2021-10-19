a0300cd8c0695ee7678fbbf89302adf3d38d023c
Jorge Melero <jormeca@gmail.com>
2021-10-19 10:49:03 +0200

    first steps of the statistics submenu

--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
 
 
+from datetime import date
 from random import sample
+from tkinter import font
 from Sim_company import *
 from Evaluations import *
 from tkinter import *
@@ -11,8 +13,10 @@ class Menu(Frame):
    def __init__(self, master=None):
        super().__init__(master)
        self.master = master
-       self.first_try_comp = True
-       self.first_try_formats = True
+       self.first_try = True
+       #self.first_try_comp = True
+       #self.first_try_formats = True
+
        self.main_menu_widgets()
       
        #self.pack(padx=20, pady=10)
@@ -54,7 +58,7 @@ class Menu(Frame):
       self.EstLabel = Label(self.frame_left, text = "Análisis de resultados", font = ("Open Sans", 9))
       self.EstLabel.pack(side = 'top')
 
-      self.EstButton = Button(self.frame_left, text = "Ver estadísticas")
+      self.EstButton = Button(self.frame_left, text = "Ver estadísticas", command= self.menu_last_evaluation)
       self.EstButton.pack(side = 'top')
 
       self.EmptyLabel2 = Label(self.frame_left)
@@ -83,7 +87,7 @@ class Menu(Frame):
       self.EmptyLabel4 = Label(self.frame_right)
       self.EmptyLabel4.pack(pady=10)
    
-   # -------------------- Cancel - bottom side side---------------------------------------
+
 
       self.CompButton = Button(self.frame_bottom, text = "Salir", command= self.salir_programa)
       self.CompButton.pack(side = BOTTOM)
@@ -97,10 +101,12 @@ class Menu(Frame):
       self.frame_right.destroy()
       self.frame_bottom.destroy()
 
+
+# -------------------- Create formats from the interactions file menu---------------------------------------
    def set_formats(self):
 
       #if entering for the first time, drops the main menú frames
-      if self.first_try_formats:
+      if self.first_try:
          self.destroy_main_frames()
          
       
@@ -129,27 +135,23 @@ class Menu(Frame):
                       messagebox.showwarning('¡Excelente!', 'Selecciona a continuación el archivo con las interacciones')
                       ev = Evaluation()
                       ev.check_matrix(sample_evaluators)                      
-                      destroy_formats_menu()
-                      self.first_try_formats=True
-                      self.main_menu_widgets()                 
+                      back_to_main()                
                   else:
                       messagebox.showwarning('Aviso', 'Escoge al menos 3 evaluadores por colaborador')            
-                      self.first_try_formats=False
+                      self.first_try =False
                       destroy_formats_menu()
                       self.set_formats()
                       #print("\nEscoge al menos 3 evaluadores por colaborador\n")
               else:
                   sample_evaluators = 0
-                  self.first_try_formats=False
+                  self.first_try =False
                   destroy_formats_menu()
                   self.set_formats() 
           
-          
-      
       def back_to_main ():
           destroy_formats_menu()
-          self.first_try_formats=True
-          self.main_menu_widgets
+          self.first_try =True
+          self.main_menu_widgets()
       
       self.txLabel = Label(self.master, text="¿Cuántas evaluaciones se requieren por cada colaborador?")
       self.txLabel.pack(side="top")
@@ -162,11 +164,12 @@ class Menu(Frame):
       self.evcancelButton = Button(self.master, text="Cancelar", command = back_to_main)
       self.evcancelButton.pack(pady=10)
 
+# -------------------- Simulate a new company menu---------------------------------------
 
    def newcompany_menu(self):
       
       #if entering for the first time, drops the main menú frames
-      if self.first_try_comp:
+      if self.first_try:
          self.destroy_main_frames()
       
       company_size = 0
@@ -194,26 +197,29 @@ class Menu(Frame):
                      messagebox.showwarning('¡Excelente!', 'La empresa se está creando')
                      new_company = Sim_comp(company_size)
                      destroy_newcompany_menu()
-                     self.first_try_comp = True
+                     self.first_try = True
                      self.main_menu_widgets()
                      #new_company.set_company()
                  else:
                      messagebox.showwarning('Aviso', 'La empresa debe tener al menos 7 colaboradores')
-                     self.first_try_comp = False
+                     self.first_try = False
                      destroy_newcompany_menu()
                      #self.main_menu_widgets()
                      self.newcompany_menu()            
              else:
                  company_size = 0
-                 self.first_try_comp = False
+                 self.first_try = False
                  destroy_newcompany_menu() 
                  self.newcompany_menu()      
          
       
       def cancel():
          destroy_newcompany_menu()
-         self.first_try_comp = True
+        # messagebox.showwarning('Aviso', 'Volver al menu original')
          self.main_menu_widgets()
+         self.first_try = True
+
+         
    
       self.sizeLabel = Label(self.master, text= "Tamaño de la empresa (número de colaboradores)", font = ("Open Sans", 10))
       self.sizeLabel.pack(side = "top")
@@ -230,6 +236,101 @@ class Menu(Frame):
       messagebox.showwarning('¡Aviso!', 'Selecciona la carpeta que contiene los archivos a cumplimentar')
       test = Evaluation()
       test.autoevaluations()
+
+
+# -------------------- First statistics menu ---------------------------------------
+
+   def menu_last_evaluation(self):
+      
+      if self.first_try:
+         self.destroy_main_frames()
+   
+   #  Set dimensions of the screen
+      total_width = self.master.winfo_screenwidth()
+      total_height = self.master.winfo_screenheight()
+
+      submenu_title = Label(self.master, text='ESTATUS ÚLTIMA EVALUACIÓN', font = ("Open Sans", 15)).grid(row=0, columnspan=2)
+      frame1 = Frame(self.master, width= 600, height=500)
+      frame1.grid(row=1, column=0)
+      frame1.config(highlightcolor='black', highlightthickness=1)
+      frame2 = Frame(self.master, width= total_width-700, height=500)
+      frame2.grid(row=1, column=1)
+      frame2.config(highlightcolor='black', highlightthickness=1)
+      frame3 = Frame(self.master, width= 800, height=total_height-500)
+      frame3.grid(row=2, column=0)
+      frame3.config(highlightcolor='black', highlightthickness=1)
+      frame4 = Frame(self.master, width= total_width-800, height=total_height-500)
+      frame4.grid(row=2, column=1)
+      frame4.config(highlightcolor='black', highlightthickness=1)
+      frame5 = Frame(self.master, width= total_width, height=200)
+      frame5.grid(row=3, columnspan=2)
+      frame5.config(highlightcolor='black', highlightthickness=1)
+
+# First Frame includes checking of formats status and of evaluators
+
+      dateLabel = Label(frame1, text="Fecha", font=('Open Sans', 20))
+      dateLabel.grid(row=0, column=0)
+      f = font.Font(dateLabel, dateLabel.cget("font"))
+      f.configure(underline=True)
+      dateLabel.configure(font = f)
+
+   #   
+   #   #get the number of evaluators required
+   #   sample_evaluators = 0
+   #   
+   #   num_evaluations = IntVar()
+   #   num_evaluations.set(7) 
+#
+   #   def destroy_formats_menu():
+   #      self.txLabel.destroy()
+   #      self.numEntry.destroy()
+   #      self.evButton.destroy()
+   #      self.evcancelButton.destroy()      
+   #   
+   #   def evaluators(event=None):
+#
+   #       try:    
+   #           sample_evaluators = num_evaluations.get()
+   #       except:
+   #           messagebox.showwarning('Aviso', 'Debes escoger un número entero')
+   #           
+   #       finally:
+   #           if isinstance(sample_evaluators, int):
+   #               if sample_evaluators >= 3:
+   #                   messagebox.showwarning('¡Excelente!', 'Selecciona a continuación el archivo con las interacciones')
+   #                   ev = Evaluation()
+   #                   ev.check_matrix(sample_evaluators)                      
+   #                   back_to_main()                
+   #               else:
+   #                   messagebox.showwarning('Aviso', 'Escoge al menos 3 evaluadores por colaborador')            
+   #                   self.first_try =False
+   #                   destroy_formats_menu()
+   #                   self.set_formats()
+   #                   #print("\nEscoge al menos 3 evaluadores por colaborador\n")
+   #           else:
+   #               sample_evaluators = 0
+   #               self.first_try =False
+   #               destroy_formats_menu()
+   #               self.set_formats() 
+   #       
+   #   def back_to_main ():
+   #       destroy_formats_menu()
+   #       self.first_try =True
+   #       self.main_menu_widgets()
+   
+         
+   #   self.txLabel = Label(self.master, text="¿Cuántas evaluaciones se requieren por cada colaborador?")
+   #   self.txLabel.pack(side="top")
+   #   self.numEntry = Entry(self.master, textvariable = num_evaluations)
+   #   self.numEntry.pack(side='top')
+   #   self.evButton = Button(self.master, text="iniciar", command = evaluators)
+   #   self.evButton.focus()
+   #   self.evButton.bind('<Return>', evaluators)
+   #   self.evButton.pack(side='top')
+   #   self.evcancelButton = Button(self.master, text="Cancelar", command = back_to_main)
+   #   self.evcancelButton.pack(pady=10)
+
+
 
    # menu =int(input("""¿What do you want to do?:
    # 1. Simulate a new company \n
